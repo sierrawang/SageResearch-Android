c313b19f77742727738c07d2de65221e3c4eb0c4
Joshua Liu <liujoshua@users.noreply.github.com>
2018-05-30 12:57:21 -0700

    MPower module builds

--- mpower/src/main/java/org/sagebionetworks/research/mpower/inject/MPowerAutoValueTypeAdapterFactory.java
+++ mpower/src/main/java/org/sagebionetworks/research/mpower/inject/MPowerAutoValueTypeAdapterFactory.java
@@ -30,5 +30,20 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-include ':app', ':domain', ':presentation', ':mobile-ui', ':data', ':mpower'
+package org.sagebionetworks.research.mpower.inject;
 
+import com.google.gson.TypeAdapterFactory;
+import com.ryanharter.auto.value.gson.GsonTypeAdapterFactory;
+
+/**
+ * Auto-generated TypeAdapterFactory for @AutoValue classes in the :app module. Due to how the auto-value-gson
+ * annotationProcessor works, a separate @GsonTypeAdapterFactory is needed for each gradle module.
+ */
+@GsonTypeAdapterFactory
+public abstract class MPowerAutoValueTypeAdapterFactory implements TypeAdapterFactory {
+    //     Static factory method to access the package
+    //     private generated implementation
+    public static TypeAdapterFactory create() {
+        return new AutoValueGson_MPowerAutoValueTypeAdapterFactory();
+    }
+}
