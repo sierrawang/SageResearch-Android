b74362f77d7753c25157a604c50d96798bb800c8
Robert Kolmos <robert.kolmos@sagebase.org>
2018-07-05 17:43:55 -0700

    Add ShowCompletionStepFragment

--- mobile-ui/src/main/java/org/sagebionetworks/research/mobile_ui/inject/ShowStepFragmentModule.java
+++ mobile-ui/src/main/java/org/sagebionetworks/research/mobile_ui/inject/ShowStepFragmentModule.java
@@ -34,11 +34,14 @@ package org.sagebionetworks.research.mobile_ui.inject;
 
 import android.support.annotation.NonNull;
 
+import org.sagebionetworks.research.domain.step.implementations.CompletionStepBase;
 import org.sagebionetworks.research.mobile_ui.show_step.view.ShowActiveUIStepFragment;
+import org.sagebionetworks.research.mobile_ui.show_step.view.ShowCompletionStepFragment;
 import org.sagebionetworks.research.mobile_ui.show_step.view.ShowStepFragmentBase;
 import org.sagebionetworks.research.mobile_ui.show_step.view.ShowUIStepFragment;
 import org.sagebionetworks.research.presentation.inject.DrawableModule;
 import org.sagebionetworks.research.presentation.model.implementations.ActiveUIStepViewBase;
+import org.sagebionetworks.research.presentation.model.implementations.CompletionStepViewBase;
 import org.sagebionetworks.research.presentation.model.implementations.UIStepViewBase;
 import org.sagebionetworks.research.presentation.model.interfaces.ActiveUIStepView;
 import org.sagebionetworks.research.presentation.model.interfaces.StepView;
@@ -90,5 +93,12 @@ public class ShowStepFragmentModule {
     @StepViewKey(UIStepViewBase.TYPE)
     static ShowStepFragmentFactory provideShowUIStepFragmentFactory() {
         return ShowUIStepFragment::newInstance;
+    }
+
+    @Provides
+    @IntoMap
+    @StepViewKey(CompletionStepViewBase.TYPE)
+    static ShowStepFragmentFactory provideShowCompletionStepFragmentFactory() {
+        return ShowCompletionStepFragment::newInstance;
     }
 }
