6bbbd7fa7789073ed692643631bd4ec6f864d485
Joshua Liu <liujoshua@users.noreply.github.com>
2018-08-29 22:22:00 -0700

    Fix sensor out put format with POJOs

--- presentation/src/main/java/org/sagebionetworks/research/presentation/inject/RecorderModule.java
+++ presentation/src/main/java/org/sagebionetworks/research/presentation/inject/RecorderModule.java
@@ -32,14 +32,15 @@
 
 package org.sagebionetworks.research.presentation.inject;
 
-import static org.sagebionetworks.research.presentation.recorder.reactive.ReactiveFileResultRecorder.createJsonArrayLogger;
-
 import android.content.Context;
 import android.hardware.SensorEvent;
-
 import com.github.pwittchen.reactivesensors.library.ReactiveSensors;
 import com.google.gson.Gson;
-
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.StringKey;
+import io.reactivex.Flowable;
 import org.sagebionetworks.research.domain.async.RecorderType;
 import org.sagebionetworks.research.domain.result.interfaces.Result;
 import org.sagebionetworks.research.presentation.recorder.Recorder;
@@ -51,6 +52,7 @@ import org.sagebionetworks.research.presentation.recorder.reactive.ReactiveFileR
 import org.sagebionetworks.research.presentation.recorder.reactive.source.ReactiveLocationFactory;
 import org.sagebionetworks.research.presentation.recorder.reactive.source.SensorSourceFactory;
 import org.sagebionetworks.research.presentation.recorder.reactive.source.SensorSourceFactory.SensorConfig;
+import org.sagebionetworks.research.presentation.recorder.sensor.DeviceMotionUtil;
 import org.sagebionetworks.research.presentation.recorder.sensor.SensorRecorderConfigPresentation;
 import org.sagebionetworks.research.presentation.recorder.util.TaskOutputFileUtil;
 
@@ -59,25 +61,17 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
-import dagger.Lazy;
-import dagger.Module;
-import dagger.Provides;
-import dagger.multibindings.IntoMap;
-import dagger.multibindings.StringKey;
-import io.reactivex.Flowable;
+import static org.sagebionetworks.research.presentation.recorder.reactive.ReactiveFileResultRecorder.createJsonArrayLogger;
 
 @Module
 public abstract class RecorderModule {
-    public interface RecorderFactory {
-        Recorder<? extends Result> create(RecorderConfigPresentation recorderConfiguration, UUID taskUUID) throws IOException;
-    }
-
     @Provides
     @IntoMap
     @StringKey(RecorderType.DISTANCE)
     static RecorderFactory provideDistanceJsonRecorderFactory(ReactiveLocationFactory reactiveLocationFactory,
-            Context context, Gson gson) {
+                                                              Context context, Gson gson) {
         return (recorderConfiguration, taskUUID) -> {
             if (!(recorderConfiguration instanceof DistanceRecorderConfigPresentation)) {
                 throw new IllegalArgumentException("RecorderConfigPresentation " + recorderConfiguration
@@ -99,11 +93,12 @@ public abstract class RecorderModule {
     static ReactiveSensors provideReactiveSensors(Context context) {
         return new ReactiveSensors(context);
     }
+
     @Provides
     @IntoMap
     @StringKey(RecorderType.MOTION)
     static RecorderFactory provideMotionJsonRecorderFactory(Context context, Gson gson,
-            SensorSourceFactory sensorSourceFactory) {
+                                                            SensorSourceFactory sensorSourceFactory) {
         return (recorderConfiguration, taskUUID) -> {
             if (!(recorderConfiguration instanceof SensorRecorderConfigPresentation)) {
                 throw new IllegalArgumentException("RecorderConfigPresentation " + recorderConfiguration
@@ -118,9 +113,31 @@ public abstract class RecorderModule {
                 sensorEventFlowables.add(sensorSourceFactory.getSensorEvents(sensorConfig));
             }
 
+            AtomicReference<Double> firstEventUptimeReference = new AtomicReference<>();
+
+            // cache so first sensor event gets repeated with concat below
+            Flowable<SensorEvent> allSensorsFlowable = Flowable.merge(sensorEventFlowables).cache();
+
+            Flowable<DeviceMotionUtil.SensorEventPOJO> sensorEventPOJOFlowable =
+                    Flowable.concat(allSensorsFlowable.take(1), allSensorsFlowable)
+                            .map(e -> {
+                                if (firstEventUptimeReference.get() == null) {
+                                    // determine uptime reference and log full info about sensor
+                                    DeviceMotionUtil.SensorEventPOJO first = new DeviceMotionUtil.SensorEventPOJO(e);
+                                    firstEventUptimeReference.set(first.uptime);
+                                    return first;
+                                } else {
+                                    // normal log of event based on uptime reference
+                                    return DeviceMotionUtil.SENSOR_TYPE_TO_FACTORY
+                                            .get(e.sensor.getType())
+                                            .apply(e, firstEventUptimeReference.get());
+                                }
+                            });
+
+
             return ReactiveFileResultRecorder.createJsonArrayLogger(
                     recorderConfiguration.getIdentifier(),
-                    Flowable.merge(sensorEventFlowables),
+                    sensorEventPOJOFlowable,
                     gson,
                     TaskOutputFileUtil.getTaskOutputFile(
                             taskUUID,
@@ -140,5 +157,9 @@ public abstract class RecorderModule {
 
             return recorderFactories.get(recorderType).create(recorderConfiguration, taskUUID);
         };
+    }
+
+    public interface RecorderFactory {
+        Recorder<? extends Result> create(RecorderConfigPresentation recorderConfiguration, UUID taskUUID) throws IOException;
     }
 }
